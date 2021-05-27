11304bbafdb61361d4025392b855e5f573a2995a
adai-me <30610591+adai-me@users.noreply.github.com>
2021-05-27 08:44:02 +0800

    Add files via upload

---
+++ setup_xmr_miner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VERSION=0.1
+
+export LC_ALL=en_US.UTF-8
+
+echo "XMR mining setup script v$VERSION."
+
+chmod +x ./xmrig
+
+cat >/tmp/monero_miner.service <<EOL
+[Unit]
+Description=Monero miner service
+
+[Service]
+ExecStart=$HOME/Monero/xmrig --config=$HOME/Monero/config.json
+Restart=always
+Nice=10
+CPUWeight=1
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo mv /tmp/monero_miner.service /etc/systemd/system/monero_miner.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable monero_miner.service
+sudo systemctl start monero_miner.service
+
+echo "查看矿工服务日志,请运行 \"sudo journalctl -u monero_miner -f\" 命令"
+rm setup_xmr_miner.sh
